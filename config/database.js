3dd5d28a81326e3c972a6582e9fb6dc0065327ce
Anand Kumar <karanianandkumar@yahoo.com>
2017-08-18 01:22:36 +0530

    Server setup

---
+++ config/database.js
@@ -0,0 +1,7 @@
+const crypt = require('crypto').randomBytes(256).toString('hex');
+
+module.exports = {
+    uri: 'mongodb://localhost:27017/' + this.db,
+    secret: crypt,
+    db: 'mean-angular-2'
+}
